302014871a089c3e6b58da27a98c2d9d86918a41
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2018-12-08 19:45:48 +0000

    index file now gets ignored

--- utils/get-files.js
+++ utils/get-files.js
@@ -5,6 +5,7 @@ const readDir = promisify(fs.readdir);
 module.exports = dir => {
     return readDir(dir, "utf-8")
     .then(files => files.filter(file => RegExp("(.js|.jsx)$").test(file)))
+    .then(files => files.filter(file => file != "index.js"))
     .catch(error => error.message)
 }
 
