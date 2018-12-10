95f1ba454ede2d32aef7cd7d6857399d276b99bb
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2018-12-10 23:27:41 +0000

    cleaned up index file.

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const path = require('path');
-const {
-    generateIndexFile,
-    getFiles,
-} = require('./utils');
-
-
+const { generateIndexFile } = require('./utils');
 
 let config = {
     DEFAULT_VERSION: 'es6',
@@ -17,8 +12,6 @@ let config = {
     }
 };
 
-let fileTreeMap = {};
-
 (async () => {
     try {
         await generateIndexFile(
@@ -28,23 +21,3 @@ let fileTreeMap = {};
         console.log(error);
     }
 })();
-
-
-async function traverseFileTreeInDir({ PATH_TO_DIR }) {
-    let files = await getFiles(PATH_TO_DIR);
-
-    for (let fileIndex in files) {
-        let file = files[fileIndex];
-        if (file.isDirectory()) {
-            await traverseFileTreeInDir({
-                PATH_TO_DIR: `${PATH_TO_DIR}/${file.name}`
-            })
-        } else {
-            if (fileTreeMap[PATH_TO_DIR]) {
-                fileTreeMap[PATH_TO_DIR].push(file.name);
-            } else {
-                fileTreeMap[PATH_TO_DIR] = [file.name];
-            }
-        }
-    }
-}
