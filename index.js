82759df18b449e1d2579970a45cf21b3a3e6fb01
Ray <rayhaan.bhikha@sky.uk>
2018-12-12 12:39:21 +0000

    test commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 const { generateIndexFile } = require('./utils');
+const babel = require("@babel/core");
 const fs = require('fs');
+const path = require('path');
+
 const program = require('commander');
 
 program
@@ -15,16 +18,34 @@ program
 let { directory, ecmaScript } = program;
 (async () => {
     try {
-        // check directory.
-        if (!directory) helperMessage("Please specifiy a directory")
-        fs.statSync(directory).isDirectory(); // throws error if not a directory.
 
-        // make ecmaScript version => es6 by default if wrong argument is passed.
-        if (ecmaScript !== 'es5' && ecmaScript !== 'es6') ecmaScript = 'es6';
+        let code = fs.readFileSync(path.resolve(directory), 'utf-8');
 
-        console.log(ecmaScript, directory);
+        const {ast} = babel.transformSync(code, {ast: true, code: false});
 
-        await generateIndexFile(directory, ecmaScript);
+        let nodesOfInterest = ast.program.body.filter(node => node.type == 'ExpressionStatement');
+        
+        // console.log(nodesOfInterest);
+        nodesOfInterest.forEach(node => {
+            console.log(node.expression);
+        })
+
+
+
+
+
+
+
+        // // check directory.
+        // if (!directory) helperMessage("Please specifiy a directory")
+        // fs.statSync(directory).isDirectory(); // throws error if not a directory.
+
+        // // make ecmaScript version => es6 by default if wrong argument is passed.
+        // if (ecmaScript !== 'es5' && ecmaScript !== 'es6') ecmaScript = 'es6';
+
+        // console.log(ecmaScript, directory);
+
+        // await generateIndexFile(directory, ecmaScript);
     } catch (error) {
         console.log(error.message);
     }
