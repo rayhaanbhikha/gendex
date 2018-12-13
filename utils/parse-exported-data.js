7685e297ab4816dba8de30935a42f33731a5fefd
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2018-12-13 18:27:34 +0000

    refactored code.

--- utils/parse-exported-data.js
+++ utils/parse-exported-data.js
@@ -1,11 +1,6 @@
-function importStructure({ ExportDefaultDeclaration, ExportNamedDeclaration, source }) {
-
-}
-
 function parseExportedData(data, version) {
-    let importStatementsUsed = {}
     let importStatements = [];
-    let importStructures = [];
+    let importStatementsUsed = {}
 
     data.forEach(node => {
         let { ExportDefaultDeclaration, ExportNamedDeclaration, source, version: fileVersion } = node;
@@ -14,17 +9,25 @@ function parseExportedData(data, version) {
             throw new Error(`Cannot create ${version} index.js, as ${source} is ${fileVersion}`);
         }
 
-        console.log({ ExportDefaultDeclaration, ExportNamedDeclaration, source, version: fileVersion })
-
+        // default exports
         if (ExportDefaultDeclaration) {
-            importStatementsUsed[ExportDefaultDeclaration] = ExportDefaultDeclaration;
+            let newImport = {
+                name: ExportDefaultDeclaration,
+                alias: ExportDefaultDeclaration,
+                source,
+                type: 'default'
+            };
+            // default statements should be unique as they rely on file name
+            importStatements.push(newImport);
         }
 
+        // named exports.
         if (ExportNamedDeclaration) {
             ExportNamedDeclaration.forEach(namedExport => {
                 let newImport = getImport({
                     name: namedExport,
-                    source
+                    source,
+                    type: 'named',
                 }, importStatements);
                 importStatements.push(newImport);
             })
@@ -32,16 +35,120 @@ function parseExportedData(data, version) {
     })
 
 
+    importStatements.forEach(importStatement => {
+        switch (importStatement.type) {
+            case 'named':
+                if (importStatementsUsed[importStatement.source]) {
+                    importStatementsUsed[importStatement.source].named.push(importStatement);
+                } else {
+                    importStatementsUsed[importStatement.source] = {
+                        named: [importStatement],
+                        default: null
+                    }
+                }
+                break;
+            case 'default':
+                if (importStatementsUsed[importStatement.source]) {
+                    importStatementsUsed[importStatement.source].default = importStatement;
+                } else {
+                    importStatementsUsed[importStatement.source] = {
+                        named: [],
+                        default: importStatement
+                    }
+                }
+                break;
+        }
+    })
 
 
+    return buildResponse(importStatements, importStatementsUsed, version);
 
-    console.log(importStatements.sort(customSorter));
+}
 
-    // console.log("\n\n============================");
-    // console.log(importStatementsUsed);
-    // if (importStatements.length === 0) { return null }
-    // return buildResponse(importStatements, importStatementsUsed, version)
+const buildResponse = (importStatements, importStatementsUsed, version) => {
+    let response = '';
 
+    // build import statments.
+    for (let source in importStatementsUsed) {
+        let { named: namedImports, default: defaultImport } = importStatementsUsed[source];
+        if (version === 'es5') {
+            response += buildImportStatementEs5(defaultImport, namedImports, source)
+        } else if (version === 'es6') {
+            response += buildImportStatementEs6(defaultImport, namedImports, source);
+        }
+        response += "\n";
+    }
+
+    response += "\n\n";
+    response += buildExportStatement(importStatements, version);
+    return response;
+}
+
+
+const buildExportStatement = (importStatements, version) => {
+    let exportStatments = importStatements.map(({ alias }) => alias);
+
+    let response = null;
+    if (version == 'es5') {
+        response = "module.exports = {\n"
+    } else if (version == 'es6') {
+        response = "export {\n"
+    }
+    exportStatments.forEach(exportItem => response += `\t${exportItem},\n`);
+    response += "}"
+    return response;
+}
+
+const buildImportStatementEs5 = (defaultImport, namedImports, source) => {
+
+    if (defaultImport) {
+        return `const ${defaultImport.name} = require(${source});`;
+    }
+
+    let namedImportsLength = namedImports.length;
+    if (namedImportsLength > 0) {
+        let importString = `const {`;
+        namedImports.forEach(({ name, alias }, index) => {
+            if (alias !== name) {
+                importString += ` ${name}: ${alias}`
+            } else {
+                importString += ` ${name}`
+            }
+            if (index !== namedImportsLength - 1) importString += ',';
+        })
+        importString += `} = require(${source});`
+        return importString;
+    }
+
+    return '';
+}
+
+const buildImportStatementEs6 = (defaultImport, namedImports, source) => {
+    let importString = 'import ';
+
+    if (defaultImport) {
+        importString += `${defaultImport.name} `;
+    }
+
+    let namedImportsLength = namedImports.length;
+
+    if (defaultImport && namedImportsLength > 0) importString += ', '
+
+    if (namedImportsLength > 0) {
+        importString += `{`;
+        namedImports.forEach(({ name, alias }, index) => {
+            if (alias !== name) {
+                importString += ` ${name} as ${alias}`
+            } else {
+                importString += ` ${name}`
+            }
+            if (index !== namedImportsLength - 1) importString += ',';
+        })
+        importString += '}'
+    }
+
+    importString += ` from ${source};`
+    return importString;
 }
 
 const customSorter = (firstImport, secondImport) => {
@@ -56,7 +163,7 @@ const customSorter = (firstImport, secondImport) => {
 
 const getImport = (newImport, importStatements) => {
 
-    let { name, source } = newImport
+    let { name, source, type } = newImport
 
     importStatements = importStatements.sort(customSorter)
 
@@ -69,6 +176,7 @@ const getImport = (newImport, importStatements) => {
     if (matches.length === 0) return {
         name,
         alias: name,
+        type,
         source
     }
 
@@ -85,146 +193,9 @@ const getImport = (newImport, importStatements) => {
     return {
         name,
         alias: newAlias,
-        source
+        source,
+        type
     }
 }
-
-// let importStatements = [
-//     { name: 'a', alias: 'a', source: './index.js' },
-//     { name: 'a', alias: 'a_2', source: './index.js' },
-//     { name: 'a', alias: 'a_1', source: './index.js' },
-//     { name: 'a', alias: 'a_4', source: './index.js' },
-//     { name: 'a', alias: 'a_5', source: './index.js' },
-//     { name: 'a', alias: 'a_3', source: './index.js' },
-//     { name: 'b', alias: 'b_4', source: './index.js' },
-//     { name: 'b', alias: 'b_6', source: './index.js' },
-//     { name: 'b', alias: 'b_3', source: './index.js' }
-// ]
-
-// let source = './index.b.js';
-// ExportNamedDeclaration.forEach(namedExport => {
-//     let newImport = getImport({
-//         name: namedExport,
-//         source
-//     }, importStatements);
-//     importStatements.push(newImport);
-// })
-
-// console.log(importStatements.sort(customSorter));
-
-const getNextIndex = (name, importStatementsUsed) => {
-    let importStatementsUsedArray = Object.getOwnPropertyNames(importStatementsUsed);
-
-    let r = `^${name}\\_*\\d*$`
-    let regex = RegExp(r, 'g');
-
-
-    let matches = importStatementsUsedArray.filter(importName => importName.search(regex) > -1)
-
-    let lastIndex = matches.reverse()[0]
-
-    let newR = /(\w*_)(\d*)$/g
-    let regexResult = newR.exec(lastIndex);
-
-    let newIndex = regexResult ?
-        `${regexResult[1]}${Number(regexResult[2]) + 1}`
-        :
-        `${lastIndex}_1`;
-
-    return newIndex
-}
-
-const buildImportStatement = (name, source, version) => {
-    return (version == 'es5') ?
-        `const ${name} = require(${source})`
-        :
-        `import ${name} from ${source}`;
-}
-
-const buildExportStatement = (exportStatements, version) => {
-    let response = null;
-    if (version == 'es5') {
-        response = "module.exports = {\n"
-    } else if (version == 'es6') {
-        response = "export {\n"
-    }
-    Object.keys(exportStatements).forEach(exportItem => response += `\t${exportItem},\n`);
-    response += "}"
-    return response;
-}
-
-const buildResponse = (importStatements, importStatementsUsed, version) => {
-    let response = importStatements.join("\n");
-    response += "\n\n\n"
-    response += buildExportStatement(importStatementsUsed, version);
-    return response;
-}
-
-const checkImport = (importDefaultName, importStatementsUsed) => {
-    if (importStatementsUsed[importDefaultName]) {
-        let newImportDefaultName = getNextIndex(importDefaultName, importStatementsUsed);
-        let displayName = `default as ${newImportDefaultName}`;
-        importStatementsUsed[newImportDefaultName] = displayName;
-        return [displayName]
-    }
-
-    importStatementsUsed[importDefaultName] = importDefaultName;
-    return importDefaultName
-}
-
-const checkImportArray = (importNames, importStatementsUsed) => {
-    return importNames.map(name => {
-        if (importStatementsUsed[name]) {
-            let newName = getNextIndex(name, importStatementsUsed)
-            let displayName = `${name} as ${newName}`
-            importStatementsUsed[newName] = newName
-            return displayName
-        }
-        importStatementsUsed[name] = name
-        return name
-    })
-}
-
-
-// // if there is an export default and export.
-// if (ExportDefaultDeclaration && ExportNamedDeclaration.length > 0) {
-
-
-//     let importDisplayName = checkImport(ExportDefaultDeclaration, importStatementsUsed)
-//     let importStatement = null;
-
-
-//     if (Array.isArray(importDisplayName)) {
-
-//         let importByNames = checkImportArray(ExportNamedDeclaration, importStatementsUsed)
-//         importByNames.unshift(importDisplayName[0]);
-//         importByNames = importByNames.join(", ");
-
-
-//         importStatement = buildImportStatement(
-//             `{${importByNames}}`,
-//             source,
-//             version
-//         )
-//     } else {
-//         importStatement = buildImportStatement(
-//             `${importDisplayName}, { ${checkImportArray(ExportNamedDeclaration, importStatementsUsed).join(', ')} }`,
-//             source,
-//             version
-//         )
-//     }
-
-//     importStatements.push(importStatement)
-// } else if (ExportDefaultDeclaration) {
-//     let importStatement = buildImportStatement(checkImport(ExportDefaultDeclaration, importStatementsUsed), source, version);
-//     importStatements.push(importStatement)
-// } else if (ExportNamedDeclaration.length > 0) {
-//     let importStatement = buildImportStatement(
-//         `{ ${checkImportArray(ExportNamedDeclaration, importStatementsUsed).join(', ')} }`,
-//         source,
-//         version
-//     )
-//     importStatements.push(importStatement);
-// }
 
 module.exports = parseExportedData;
