f081665bb5098410eb9f8a265708b64e19a8a133
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2018-12-14 20:03:01 +0000

    renamed utils folder to src

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { generateIndexFile } = require('./utils');
+const { generateIndexFile } = require('./src');
 const fs = require('fs');
 const program = require('commander');
 
