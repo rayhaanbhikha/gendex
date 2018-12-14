1debb0ff7c53983f49e04662160bf3d9662d1848
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2018-12-14 20:46:49 +0000

    ammended regular expression

--- src/check-version.js
+++ src/check-version.js
@@ -1,11 +1,10 @@
 const checkJsVerson = fileCode => {
-    let resEs5Expression = '((module.exports)|(exports.(\w+)))(\ .*|\t.*)='
-    let resEs6Expression = '(export\ )'
+    let resEs5Expression = /(module.exports|exports\.\w+)(\ *|\t*)=/
+    let resEs6Expression = /export\ /
+    let regEs5 = RegExp(resEs5Expression, 'gm')
+    let regEs6 = RegExp(resEs6Expression, 'gm');
 
-    let regEs5 = RegExp(resEs5Expression,'g')
-    let regEs6 = RegExp(resEs6Expression, 'g');
-    
-    if(regEs5.test(fileCode)) {
+    if (regEs5.test(fileCode)) {
         return 'es5';
     } else if (regEs6.test(fileCode)) {
         return 'es6';
