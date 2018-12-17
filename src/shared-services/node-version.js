72f02d78f44167d3a8cfab1cf803fe6edabd7058
Ray <rayhaan.bhikha@sky.uk>
2018-12-17 15:23:08 +0000

    return number

--- src/shared-services/node-version.js
+++ src/shared-services/node-version.js
@@ -1,6 +1,6 @@
 module.exports = (() => {
-    let versionRegex = /^v(\d+\.\d+\.\d+)/; // v10.10.0
+    let versionRegex = /^v(\d+\.\d+)/; // v10.10.0
     let version = versionRegex.exec(process.version)[1]
 
-    return version
+    return Number(version)
 })()
