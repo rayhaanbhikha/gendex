d22bb77d40cc6145ac6f40f4c3719933d419adbc
Ray <rayhaan.bhikha@sky.uk>
2018-12-17 15:00:09 +0000

    nodeversion service

--- src/shared-services/index.js
+++ src/shared-services/index.js
@@ -1,8 +1,10 @@
 const extractExports = require("./extract-exports.js");
 const getFileName = require("./get-file-name.js");
+const nodeVersion = require("./node-version");
 
 
 module.exports = {
 	extractExports,
 	getFileName,
+	nodeVersion
 }
