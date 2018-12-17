0847c10d1198d29f4a7c00f11ebe0cdb6f787797
Ray <rayhaan.bhikha@sky.uk>
2018-12-17 15:23:24 +0000

    get files updated

--- src/get-files/get-files.js
+++ src/get-files/get-files.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { promisify } = require('util');
+const {nodeVersion} = require("../shared-services")
 const readDir = promisify(fs.readdir);
 
 let options = {
@@ -18,7 +19,11 @@ async function getFiles(dir, filterOptions = {}) {
     try {
         let files = await readDir(dir, options);
 
-        files = filterByFilesAndFolders(files);
+        if(nodeVersion >= 10.10) {
+            files = filterByFilesAndFolders(files);
+        } else {
+            files = filterByFilesAndFoldersV8(dir, files);
+        }
 
         if (filterOptions.excludeFolders) files = filterByFilesOnly(files);
 
@@ -41,6 +46,30 @@ function filterByFilesAndFolders(files) {
         && generalFileExclusions.indexOf(file.name) === -1 // exclude files listed in 'general exclusions'
         && regExExclusions.test(file.name) === false // exclude test files
     )
+}
+
+function filterByFilesAndFoldersV8(dir, files) {
+    return files.map(file => {
+
+        // exclude files listed in 'general exclusions'
+        // exclude test files)
+        if( generalFileExclusions.indexOf(file) === -1  && regExExclusions.test(file) === false) {
+
+            // the return object ensures other functions consuming this function can check if the file is a directory.
+            // allows backwards compatibility between (fs.dirent >= v10.10.0) and (fs.statsync < v10.10.0)
+            if(RegExp("(.js|.jsx)$").test(file)) {
+                return {
+                    name: file,
+                    isDirectory: () => false
+                }
+            } else if(fs.statSync(`${dir}/${file}`).isDirectory()) {
+                return {
+                    name: file,
+                    isDirectory: () => true
+                }
+            }
+        }
+    }).filter(fileObject => fileObject);
 }
 
 function filterByFilesOnly(files) {
